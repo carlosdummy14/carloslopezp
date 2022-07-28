fa2e7324dffad4202fa3d8590ae60ca9875b7d2d
carlos <carlosdummy14@gmail.com>
2022-07-27 22:18:09 -0500

    fix: first responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,12 @@ import rockImage from '../images/rock-zen.png'
 function App() {
   return (
     <div className={styles.container}>
-      <header className={styles.hero}>
+      <section className={styles.hero}>
         <div className={styles.image}>
           <img src={carlosImage} alt='carlos lopez' />
         </div>
         <div className={styles.info}>
-          <div className={styles.greeting}>
-            <span>Hi, I'm</span>
-          </div>
+          <div className={styles.greeting}>Hi, I'm</div>
           <h1 className={styles.title}>
             <span className={styles.firstname}>Carlos</span>
             <span className={styles.lastname}>López</span>
@@ -33,53 +31,53 @@ function App() {
             </a>
           </div>
         </div>
-      </header>
+      </section>
 
-      <section className={styles.sectioncontainer}>
-        <article className={styles.aboutme}>
-          <div>
-            <h3>About me</h3>
-            <h6>"Do it, but make it worth it"</h6>
-            <p>
-              <span className={styles['main-text']}>
-                Full stack developer. HTML, CSS, JavaScript, React, Nodejs, Express, MongoDB -
-                mainly.
-              </span>
-              <br />
-              <br />
-              Love learning something new every day. 📖
-              <br />
-              I help small businesses to be part of a digital world. 🌎
-              <br />
-              I have worked with PHP, SQL and other languages.
-              <br />
-              I have been involved in the different areas of the organizations where I have worked,
-              either developing or implementing software products.
-              <br />
-              I was part of systems development teams for accounting, finance, human resources,
-              payroll, inventories, purchases and manufacturing.
-              <br />
-              The contact with the end user allowed me to identify how a software should be
-              according to their needs.
-              <br />
-              <br />
-              Grateful to God. ✨
-              <br />
-              Father and I love my family. 💙
-              <br />I like soccer ⚽, board games 🧩 and Magic The Gathering TCG. ⚔
-            </p>
-          </div>
-          <div className={styles['rock-container']}>
-            <div className={styles['image-circle']}></div>
-            <img
-              src={rockImage}
-              alt='Applied rocks mean balance'
-              width='600px'
-              height='580px'
-              className={styles.rock}
-            />
-          </div>
-        </article>
+      <section className={styles.aboutme}>
+        <div className={styles.abouttext}>
+          <h3>About me</h3>
+          <h6>"Do it, but make it worth it"</h6>
+          <p>
+            <span className={styles['main-text']}>
+              Full stack developer. HTML, CSS, JavaScript, React, Nodejs, Express, MongoDB - mainly.
+            </span>
+            <br />
+            <br />
+            Love learning something new every day. 📖
+            <br />
+            I help small businesses to be part of a digital world. 🌎
+            <br />
+            I have worked with PHP, SQL and other languages.
+            <br />
+            I have been involved in the different areas of the organizations where I have worked,
+            either developing or implementing software products.
+            <br />
+            I was part of systems development teams for accounting, finance, human resources,
+            payroll, inventories, purchases and manufacturing.
+            <br />
+            The contact with the end user allowed me to identify how a software should be according
+            to their needs.
+            <br />
+            <br />
+          </p>
+          <h6>"Making code is not everything, you also have to solve the problem and have fun"</h6>
+          <p>
+            Grateful to God. ✨
+            <br />
+            Father and I love my family. 💙
+            <br />I like soccer ⚽, board games 🧩 and Magic The Gathering TCG. ⚔
+          </p>
+        </div>
+        <div className={styles['rock-container']}>
+          <div className={styles['image-circle']}></div>
+          <img
+            src={rockImage}
+            alt='Applied rocks mean balance'
+            width='60px'
+            height='58px'
+            className={styles.rock}
+          />
+        </div>
       </section>
     </div>
   )
